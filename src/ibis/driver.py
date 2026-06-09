8c0cc9dddc461bfedbf3f9766309a3f07bbeb9ef
Jon Palmisciano <jp@jonpalmisc.com>
2026-06-10 07:50:05 +1000

    Add support for iOS 27 iBoot/mBoot
    Resolves: https://github.com/jonpalmisc/ibis/issues/22

--- src/ibis/driver.py
+++ src/ibis/driver.py
@@ -7,8 +7,25 @@ from ibis.context import Context
 
 
 class Driver(ABC):
-    _BANNER_OFFSET = 0x200
-    _BUILD_TAG_OFFSET = 0x280
+    # XXX: Do not use these directly, use the property functions instead; a
+    # global shift need to be applied.
+    _BANNER_OFFSET_RAW = 0x200
+    _BUILD_TAG_OFFSET_RAW = 0x280
+    _LAYOUT_TABLE_OFFSET_RAW = 0x300
+
+    _global_shift: int = 0
+
+    @property
+    def build_tag_offset(self) -> int:
+        return self._BUILD_TAG_OFFSET_RAW + self._global_shift
+
+    @property
+    def banner_offset(self) -> int:
+        return self._BANNER_OFFSET_RAW + self._global_shift
+
+    @property
+    def layout_table_offset(self) -> int:
+        return self._LAYOUT_TABLE_OFFSET_RAW + self._global_shift
 
     @abstractmethod
     def read(self, offset: int, size: int) -> bytes: ...
@@ -20,8 +37,15 @@ class Driver(ABC):
         return self.read(offset, size).split(b"\x00")[0].decode().rstrip("\x00")
 
     def detect_context(self) -> Context:
-        banner = self.read_str(self._BANNER_OFFSET, 0x40)
-        tag = self.read_str(self._BUILD_TAG_OFFSET, 0x40)
+        # Traditionally the copyright banner has always been at 0x200. Starting
+        # with iOS 27, it is pushed back by 0x80. Probing 0x200 for a string
+        # containing "Apple" should tell us which format we have.
+        if "Apple" not in self.read_str(self._BANNER_OFFSET_RAW, 0x40):
+            self._global_shift = 0x80
+            logging.debug("Detected iOS 27+ metadata layout.")
+
+        banner = self.read_str(self.banner_offset, 0x40)
+        tag = self.read_str(self.build_tag_offset, 0x40)
 
         logging.debug(f"Found banner: {banner}")
         logging.debug(f"Found build tag: {tag}")
